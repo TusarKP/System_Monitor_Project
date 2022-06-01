38759be9d3e74f734ccea65dec63152b6b9041c5
Tusarkanta Palauri <tusarxml@gmail.com>
2022-06-01 23:25:30 +0530

    Project Files Added
    Solutions for System Monitor Project

---
+++ src/format.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <sstream>
+#include <iomanip>
+#include "format.h"
+
+using std::string;
+using std::stringstream;
+
+string Format::ElapsedTime(long seconds) {
+    int hh, mm, ss;
+    stringstream output;
+    hh = (seconds / 3600) % 100;
+    mm = (seconds / 60) % 60;
+    ss = seconds % 60;
+    output  << std::setfill('0') << std::setw(2) << hh << ":"
+            << std::setfill('0') << std::setw(2) << mm << ":"
+            << std::setfill('0') << std::setw(2) << ss;
+    return output.str();
+}
